ccd826df2af97d9ecc499e035d951319b814e29f
IR0NSIGHT <irnsght@googlemail.com>
2024-08-22 19:56:21 +0200

    add baseRadius and radnom fluctuate (dead) gui components)

--- src/main/java/org/demo/wpplugin/operations/ApplyPathOperationOptions.java
+++ src/main/java/org/demo/wpplugin/operations/ApplyPathOperationOptions.java
@@ -4,8 +4,28 @@ import org.pepsoft.worldpainter.operations.OperationOptions;
 import org.pepsoft.worldpainter.selection.CopySelectionOperation;
 import org.pepsoft.worldpainter.selection.SelectionOptions;
 
-public class ApplyPathOperationOptions extends SelectionOptions implements OperationOptions<CopySelectionOperation> {
+public class ApplyPathOperationOptions implements OperationOptions<CopySelectionOperation> {
     private int stepsPerGrowth = 0;
+
+    private double randomFluctuate = 0f;
+
+    public double getRandomFluctuate() {
+        return randomFluctuate;
+    }
+
+    public void setRandomFluctuate(double randomFluctuate) {
+        this.randomFluctuate = randomFluctuate;
+    }
+
+    public int getBaseRadius() {
+        return baseRadius;
+    }
+
+    public void setBaseRadius(int baseRadius) {
+        this.baseRadius = baseRadius;
+    }
+
+    private int baseRadius = 1;
 
     public int getStepsPerGrowth() {
         return stepsPerGrowth;
--- src/main/java/org/demo/wpplugin/operations/ApplyPathOperationOptionsPanel.java
+++ src/main/java/org/demo/wpplugin/operations/ApplyPathOperationOptionsPanel.java
@@ -7,8 +7,14 @@ package org.demo.wpplugin.operations;//
 import javax.swing.*;
 
 public class ApplyPathOperationOptionsPanel extends JPanel {
+    private JLabel labelRandomPercent;
+    private JSpinner spinnerRandomPercent;
+
+    private JLabel labelBaseRadius;
+    private JSpinner spinnerBaseRadius;
+
     private JLabel labelGrowthPerStep;
-    private JSpinner spinnerGrowthPerStep;
+    private JSpinner spinnerStepPerGrowth;
     private ApplyPathOperationOptions options;
 
     public ApplyPathOperationOptionsPanel(ApplyPathOperationOptions options) {
@@ -22,32 +28,63 @@ public class ApplyPathOperationOptionsPanel extends JPanel {
     }
 
     private void initComponents() {
+        labelRandomPercent = new JLabel();
+        labelRandomPercent.setText("random fluctuation");
+        labelRandomPercent.setToolTipText("each step the rivers radius will randomly increase or decrease by x percent. Be very conservative because of compounding effects.");
+
+        spinnerRandomPercent = new javax.swing.JSpinner();
+        spinnerRandomPercent.setModel(new javax.swing.SpinnerNumberModel(2, 0, 100, 1));
+        spinnerRandomPercent.setEnabled(true);
+        spinnerRandomPercent.addChangeListener(evt -> options.setRandomFluctuate(((int) spinnerRandomPercent.getValue()) / 100d));
+
+
+        labelBaseRadius = new JLabel();
+        labelBaseRadius.setText("base radius");
+        labelBaseRadius.setToolTipText("base number how thick the path will be. growth and random fluctuation is added on top");
+
+        spinnerBaseRadius = new javax.swing.JSpinner();
+        spinnerBaseRadius.setModel(new javax.swing.SpinnerNumberModel(3, 0, 100, 1));
+        spinnerBaseRadius.setEnabled(true);
+        spinnerBaseRadius.addChangeListener(evt -> options.setBaseRadius((int) spinnerBaseRadius.getValue()));
+
+
         labelGrowthPerStep = new JLabel();
         labelGrowthPerStep.setText("steps per growth");
         labelGrowthPerStep.setToolTipText("how many steps it along the path it takes for an increase of 1 in width. Zero for ignore");
 
-        spinnerGrowthPerStep = new javax.swing.JSpinner();
-        spinnerGrowthPerStep.setFont(spinnerGrowthPerStep.getFont().deriveFont(spinnerGrowthPerStep.getFont().getSize() - 1f));
-        spinnerGrowthPerStep.setModel(new javax.swing.SpinnerNumberModel(0, 0, 1000, 1));
-        spinnerGrowthPerStep.setEnabled(true);
-        spinnerGrowthPerStep.addChangeListener(evt -> options.setStepsPerGrowth((int) spinnerGrowthPerStep.getValue()));
+        spinnerStepPerGrowth = new javax.swing.JSpinner();
+        spinnerStepPerGrowth.setModel(new javax.swing.SpinnerNumberModel(500, 0, 1000, 1));
+        spinnerStepPerGrowth.setEnabled(true);
+        spinnerStepPerGrowth.addChangeListener(evt -> options.setStepsPerGrowth((int) spinnerStepPerGrowth.getValue()));
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
                 layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addComponent(labelBaseRadius)
+                        .addComponent(spinnerBaseRadius)
                         .addComponent(labelGrowthPerStep)
-                        .addComponent(spinnerGrowthPerStep)
+                        .addComponent(spinnerStepPerGrowth)
+                        .addComponent(labelRandomPercent)
+                        .addComponent(spinnerRandomPercent)
         );
         layout.setVerticalGroup(
                 layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                         .addGroup(layout.createSequentialGroup()
+                                .addComponent(labelBaseRadius)
+                                .addComponent(spinnerBaseRadius)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+
                                 .addComponent(labelGrowthPerStep)
-                                .addComponent(spinnerGrowthPerStep)
+                                .addComponent(spinnerStepPerGrowth)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+
+                                .addComponent(labelRandomPercent)
+                                .addComponent(spinnerRandomPercent)
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                                 .addGap(0, 0, 0))
         );
-   }
+    }
 
     private void setControlStates() {
         //this.checkBoxRemoveExistingLayers.setEnabled(this.checkBoxLayers.isSelected());
