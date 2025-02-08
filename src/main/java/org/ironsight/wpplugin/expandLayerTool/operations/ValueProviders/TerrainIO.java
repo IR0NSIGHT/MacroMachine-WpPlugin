4b5d3f52378f8fccb3e2d1c114cce0557bdd333b
Ir0nsight <irnsght@googlemail.com>
2025-02-08 10:07:45 +0100

    add terrain getter/setter

---
+++ src/main/java/org/ironsight/wpplugin/expandLayerTool/operations/ValueProviders/TerrainIO.java
@@ -0,0 +1,64 @@
+package org.ironsight.wpplugin.expandLayerTool.operations.ValueProviders;
+
+import org.pepsoft.worldpainter.ColourScheme;
+import org.pepsoft.worldpainter.Dimension;
+import org.pepsoft.worldpainter.Terrain;
+import org.pepsoft.worldpainter.colourschemes.HardcodedColourScheme;
+
+import java.awt.*;
+
+public class TerrainIO implements IPositionValueGetter, IPositionValueSetter {
+    private final ColourScheme colorScheme = new HardcodedColourScheme();
+
+    @Override
+    public int getValueAt(Dimension dim, int x, int y) {
+        return dim.getTerrainAt(x, y).ordinal();
+    }
+
+    @Override
+    public void setValueAt(Dimension dim, int x, int y, int value) {
+        dim.setTerrainAt(x, y, Terrain.values()[value]);
+    }
+
+    @Override
+    public String getName() {
+        return "Terrain";
+    }
+
+    @Override
+    public String getDescription() {
+        return "surface terrain blocks";
+    }
+
+    @Override
+    public int getMinValue() {
+        return 0;
+    }
+
+    @Override
+    public int getMaxValue() {
+        return Terrain.values().length - 1;
+    }
+
+    @Override
+    public String valueToString(int value) {
+        return Terrain.values()[value].name();
+    }
+
+    @Override
+    public boolean isDiscrete() {
+        return true;
+    }
+
+    public int getColour(int value) {
+        Terrain t = Terrain.values()[value];
+        return t.getColour(1234L, 0, 0, 0, 0, null, colorScheme);
+    }
+
+    @Override
+    public void paint(Graphics g, int value, java.awt.Dimension dim) {
+        int color = getColour(value);
+        g.setColor(new Color(color));
+        g.fillRect(0, 0, dim.width, dim.height);
+    }
+}
