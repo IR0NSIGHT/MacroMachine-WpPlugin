af6f6f1369d3195490c88138c6abe9a73ea04dc7
Ir0nsight <irnsght@googlemail.com>
2025-03-03 20:11:44 +0100

    helpbutton action editor

--- src/main/java/org/ironsight/wpplugin/expandLayerTool/Gui/ActionEditor.java
+++ src/main/java/org/ironsight/wpplugin/expandLayerTool/Gui/ActionEditor.java
@@ -9,6 +9,8 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.function.Consumer;
 
+import static org.ironsight.wpplugin.expandLayerTool.Gui.HelpDialog.getHelpButton;
+
 public class ActionEditor extends LayerMappingPanel {
     private static JDialog dialog;
     private final Consumer<LayerMapping> onSubmit;
@@ -74,9 +76,25 @@ public class ActionEditor extends LayerMappingPanel {
             }
         });
 
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.add(submitButtom);
+        buttonPanel.add(getHelpButton("Action Editor","Use the Action editor the define what your action does.\n" +
+                "Every action has an input, and output and a type.\n" +
+                "Define which value of the input maps to which value of the output. The action can interpolate " +
+                "between values if the input/output types allow it. This is shown by the dotted line in the graph.\n" +
+                "An action will take a block from the map, read its input value and then set the blocks output value " +
+                "to the one defined by the mapping. If the mapping is defined so that slope 45° sets pine to 8, then " +
+                "all blocks with 45° slope will " +
+                "receive pine level 8 when the action is executed.\n" +
+                "Actions are part of a macro and  can be reused in multiple macros at once.\n" +
+                "Save your changes to the action to save it to your global list of actions.\n" +
+                "Add new mapping points by left clicking in the graph\n" +
+                "Delete a point by rightclicking it.\n" +
+                "Change a point by dragging it or selecting different values in the table."));
+
         this.add(topBar, BorderLayout.NORTH);
         this.add(table, BorderLayout.EAST);
-        this.add(submitButtom, BorderLayout.SOUTH);
+        this.add(buttonPanel, BorderLayout.SOUTH);
         this.add(mappingDisplay, BorderLayout.CENTER);
     }
 }
