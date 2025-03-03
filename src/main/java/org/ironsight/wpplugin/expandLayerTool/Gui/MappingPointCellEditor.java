c45ad23063d68c28e516c49f00f1cc38ba57231a
Ir0nsight <irnsght@googlemail.com>
2025-03-03 17:43:40 +0100

    dropdown for value selection of Input/Output is sorted alphabetically if discrete

--- src/main/java/org/ironsight/wpplugin/expandLayerTool/Gui/MappingPointCellEditor.java
+++ src/main/java/org/ironsight/wpplugin/expandLayerTool/Gui/MappingPointCellEditor.java
@@ -5,6 +5,8 @@ import org.ironsight.wpplugin.expandLayerTool.operations.ValueProviders.IMapping
 import javax.swing.*;
 import javax.swing.table.TableCellEditor;
 import java.awt.*;
+import java.util.ArrayList;
+import java.util.Comparator;
 
 public class MappingPointCellEditor extends DefaultCellEditor implements TableCellEditor {
     static JComboBox<MappingPointValue> dropdown = new JComboBox<>();  //FIXME will static cause problems?
@@ -20,11 +22,27 @@ public class MappingPointCellEditor extends DefaultCellEditor implements TableCe
         assert value instanceof MappingPointValue;
 
         dropdown.removeAllItems();
+
         IMappingValue mappingValue = ((MappingPointValue) value).mappingValue;
+        ArrayList<MappingPointValue> arr = new ArrayList<>(mappingValue.getMaxValue() - mappingValue.getMinValue());
         for (int i = mappingValue.getMinValue(); i <= mappingValue.getMaxValue(); i++) {
             MappingPointValue pointValue = new MappingPointValue(i, mappingValue);
-            dropdown.addItem(pointValue);
+            arr.add(pointValue);
         }
+
+        if (mappingValue.isDiscrete()) {
+            arr.sort(new Comparator<MappingPointValue>() {
+                @Override
+                public int compare(MappingPointValue o1, MappingPointValue o2) {
+                    return o1.mappingValue.valueToString(o1.numericValue).compareTo(o2.mappingValue.valueToString(o2.numericValue));
+                }
+            });
+        }
+
+        for (MappingPointValue mappingPointValue : arr) {
+            dropdown.addItem(mappingPointValue);
+        }
+
         return this.getComponent();
     }
 }
