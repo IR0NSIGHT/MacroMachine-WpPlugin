e4ceabcd2215fb1ce154ce09c948673c674c98a1
Ir0nsight <irnsght@googlemail.com>
2025-03-19 15:51:46 +0100

    remove default filter, because it was testing slope -> performance cost

--- src/main/java/org/ironsight/wpplugin/expandLayerTool/operations/ApplyAction.java
+++ src/main/java/org/ironsight/wpplugin/expandLayerTool/operations/ApplyAction.java
@@ -2,9 +2,7 @@ package org.ironsight.wpplugin.expandLayerTool.operations;
 
 import org.pepsoft.worldpainter.Dimension;
 import org.pepsoft.worldpainter.Tile;
-import org.pepsoft.worldpainter.panels.DefaultFilter;
-import org.pepsoft.worldpainter.selection.SelectionBlock;
-import org.pepsoft.worldpainter.selection.SelectionChunk;
+import org.pepsoft.worldpainter.operations.Filter;
 
 import java.awt.*;
 import java.util.Iterator;
@@ -14,12 +12,12 @@ import static org.pepsoft.worldpainter.Constants.TILE_SIZE;
 import static org.pepsoft.worldpainter.Constants.TILE_SIZE_BITS;
 
 public class ApplyAction {
-    public static void applyToDimensionWithFilter(Dimension dim, DefaultFilter filter, Consumer<Point> applyOnBlock) {
+    public static void applyToDimensionWithFilter(Dimension dim, Filter filter, Consumer<Point> applyOnBlock) {
         Iterator<? extends Tile> t = dim.getTiles().iterator();
         while (t.hasNext()) {
             Tile tile = t.next();
 
-            {   // EARLY ABORT IF TILE DOESNT MEET MINIMAL CRITERIA
+            /*{   // EARLY ABORT IF TILE DOESNT MEET MINIMAL CRITERIA
                 if ((filter.isInSelection() && !tile.hasLayer(SelectionBlock.INSTANCE) &&
                         !tile.hasLayer(SelectionChunk.INSTANCE)))
                     continue;
@@ -31,7 +29,7 @@ public class ApplyAction {
 
                 // if level below 17 vs tile lowest is 18 => 17 < 18 => reject
                 if (filter.getBelowLevel() < tile.getLowestIntHeight()) continue;
-            }
+            }*/
 
             Point p = new Point(0, 0);
             for (int yInTile = 0; yInTile < TILE_SIZE; yInTile++) {
@@ -41,7 +39,8 @@ public class ApplyAction {
                     final int y = yInTile + (tile.getY() << TILE_SIZE_BITS);
                     p.x = x;
                     p.y = y;
-                    if (0 != filter.modifyStrength(x, y, 1f)) applyOnBlock.accept(p);
+                    //if (0 != filter.modifyStrength(x, y, 1f))
+                    applyOnBlock.accept(p);
                 }
             }
         }
