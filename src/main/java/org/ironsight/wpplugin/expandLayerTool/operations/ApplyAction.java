86b23addd8806f7b16ddc92f6992868b09eda24c
Ir0nsight <irnsght@googlemail.com>
2025-03-21 16:35:11 +0100

    use early-abort-tileFilter and check Actions-readyness before applying macro with error popup

--- src/main/java/org/ironsight/wpplugin/expandLayerTool/operations/ApplyAction.java
+++ src/main/java/org/ironsight/wpplugin/expandLayerTool/operations/ApplyAction.java
@@ -15,23 +15,13 @@ import static org.pepsoft.worldpainter.Constants.TILE_SIZE;
 import static org.pepsoft.worldpainter.Constants.TILE_SIZE_BITS;
 
 public class ApplyAction {
-    public static void applyToDimensionWithFilter(Dimension dim, Filter filter, Consumer<Point> applyOnBlock) {
+
+    public static void applyToDimensionWithFilter(Dimension dim, TileFilter filter, Consumer<Point> applyOnBlock) {
         Iterator<? extends Tile> t = dim.getTiles().iterator();
         while (t.hasNext()) {
             Tile tile = t.next();
-            if (filter instanceof DefaultFilter) {   // EARLY ABORT IF TILE DOESNT MEET MINIMAL CRITERIA
-                DefaultFilter defaultFilter = (DefaultFilter) filter;
-                if ((defaultFilter.isInSelection() && !tile.hasLayer(SelectionBlock.INSTANCE) &&
-                        !tile.hasLayer(SelectionChunk.INSTANCE))) continue;
-
-                if (defaultFilter.getOnlyOnLayer() != null && !tile.hasLayer(defaultFilter.getOnlyOnLayer())) continue;
-
-                // if level above 100   vs tile max is 52 => 100 > 52 => reject
-                if (defaultFilter.getAboveLevel() > tile.getHighestIntHeight()) continue;
-
-                // if level below 17 vs tile lowest is 18 => 17 < 18 => reject
-                if (defaultFilter.getBelowLevel() < tile.getLowestIntHeight()) continue;
-            }
+            TileFilter.passType pass = filter.testTile(tile);
+            if (pass == TileFilter.passType.NO_BLOCKS) continue;
 
             Point p = new Point(0, 0);
             for (int yInTile = 0; yInTile < TILE_SIZE; yInTile++) {
@@ -40,7 +30,7 @@ public class ApplyAction {
                     final int y = yInTile + (tile.getY() << TILE_SIZE_BITS);
                     p.x = x;
                     p.y = y;
-                    if (0 != filter.modifyStrength(x, y, 1f)) applyOnBlock.accept(p);
+                    if (pass == TileFilter.passType.ALL_BLOCKS || 0 != filter.modifyStrength(x, y, 1f)) applyOnBlock.accept(p);
                 }
             }
         }
--- src/main/java/org/ironsight/wpplugin/expandLayerTool/operations/MacroDialogOperation.java
+++ src/main/java/org/ironsight/wpplugin/expandLayerTool/operations/MacroDialogOperation.java
@@ -44,13 +44,46 @@ public class MacroDialogOperation extends AbstractOperation {
         }
     }
 
+    public static void ErrorPopUp(String message) {
+        JOptionPane.showMessageDialog(null,
+                message,
+                "Error",
+                // Title of the dialog
+                JOptionPane.ERROR_MESSAGE
+                // Type of message (error icon)
+        );
+    }
+
     public void applyLayerAction(MappingMacro macro) {
         try {
-            boolean loop = macro.hasLoop(new HashSet<>());
-
             this.getDimension().setEventsInhibited(true);
             LinkedList<List<UUID>> actionIds = new LinkedList<>();
-            macro.collectActions(actionIds);
+            List<List<UUID>> steps = macro.collectActions(actionIds);
+            for (List<UUID> step : steps) {
+                for (UUID actionId : step) {
+                    LayerMapping action = LayerMappingContainer.INSTANCE.queryById(actionId);
+                    if (action == null) {
+                        ErrorPopUp("Action " + actionId + " not found.");
+                        return;
+                    }
+                    try {
+                        action.output.prepareForDimension(getDimension());
+                        action.input.prepareForDimension(getDimension());
+                    } catch (IllegalAccessError e) {    //FIXME move this check further up.
+                        JOptionPane.showMessageDialog(null,
+                                "Action "+action.getName()+" can not be applied to the map." + e.getMessage(),
+                                "Error",
+                                // Title of the dialog
+                                JOptionPane.ERROR_MESSAGE
+                                // Type of message (error icon)
+                        );
+                        return;
+                    }
+                }
+            }
+
+            // ----------------------- macro is ready and can be applied to map
+
             System.out.println("Execution order");
             actionIds.forEach(step -> {
                 StringBuilder b = new StringBuilder("Step:\n");
