8388500cd49db88f1e72778bd69fd57cf62af2eb
IR0NSIGHT <irnsght@googlemail.com>
2025-07-10 16:25:58 +0200

    refactor shadowmap

--- src/main/java/org/ironsight/wpplugin/macromachine/operations/specialOperations/ShadowMap.java
+++ src/main/java/org/ironsight/wpplugin/macromachine/operations/specialOperations/ShadowMap.java
@@ -11,35 +11,34 @@ import static org.pepsoft.worldpainter.Constants.TILE_SIZE;
 public class ShadowMap {
     public static TileContainer calculateShadowMap(Rectangle extent, TerrainHeightIO heightIO, Dimension dim) {
         TileContainer shadowmap = new TileContainer(extent,0);
+        shadowmap.addAsValues(heightIO, dim);
         for (int x = shadowmap.getMinXPos(); x < shadowmap.getMaxXPos(); x++) {
             // iterate column from south (-y) to north (+y)
-            int[] column = new int[shadowmap.getMaxYPos() - shadowmap.getMinYPos()];
-            {
-                int i = 0;
-                for (int y = shadowmap.getMaxYPos() - 1; y >= shadowmap.getMinYPos(); y--) {
-                    column[i++] = heightIO.getValueAt(dim,x,y);
-                }
-            }
+            int[] column = shadowmap.getValueColumn(x);
             int[] shadowColumn = calculateShadowFor(column);
-            {
-                int i = 0;
-                for (int y = shadowmap.getMaxYPos() - 1; y >= shadowmap.getMinYPos(); y--) {
-                    shadowmap.setValueAt(x,y,shadowColumn[i++]);
-                }
-            }
+            shadowmap.setValueColumn(shadowColumn, x);
         }
         return shadowmap;
+    }
+
+    public static TileContainer expandMask(TileContainer container) {
+        for (int y = container.getMaxYPos() - 1; y >= container.getMinYPos(); y--) {
+            //left to right, row by row
+            int[] row = container.getValueRow(y);
+
+        }
+
+        return container;
     }
 
     public static int[] calculateShadowFor(int[] heightmapColumn) {
         int[] shadowMap = new int[heightmapColumn.length];
         int shadowHeight = 0;
-        int idx = 0;
-        for (int terrainHeight: heightmapColumn) {
+        for (int i = heightmapColumn.length - 1; i >= 0; i--) {
+            int terrainHeight = heightmapColumn[i];
             shadowHeight = Math.max(shadowHeight -1, terrainHeight);
             if (shadowHeight > terrainHeight)
-                shadowMap[idx] = shadowHeight - terrainHeight;
-            idx++;
+                shadowMap[i] = shadowHeight - terrainHeight;
         }
         return shadowMap;
     }
--- src/test/java/org/ironsight/wpplugin/macromachine/operations/specialOperations/ShadowMapTest.java
+++ src/test/java/org/ironsight/wpplugin/macromachine/operations/specialOperations/ShadowMapTest.java
@@ -19,15 +19,15 @@ class ShadowMapTest {
     @Test
     void calculateShadowFor() {
         {
-            int[] terrainHeight = new int[]{0, 0, 0, 5, 0, 0, 0, 0, 0, 0};
+            int[] terrainHeight = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 5, 0};
             int[] shadowMap = ShadowMap.calculateShadowFor(terrainHeight);
-            assertArrayEquals(new int[]{0, 0, 0, 0, 4, 3, 2, 1, 0, 0}, shadowMap);
+            assertArrayEquals(new int[]{0, 0, 0, 0, 1, 2, 3, 4, 0, 0}, shadowMap);
         }
 
         {
-            int[] terrainHeight = new int[]{0, 2, 0, 5, 5, 0, 0, 0, 0, 0};
+            int[] terrainHeight = new int[]{0, 0, 0, 0, 7, 2, 0, 5, 5, 0};
             int[] shadowMap = ShadowMap.calculateShadowFor(terrainHeight);
-            assertArrayEquals(    new int[]{0, 0, 1, 0, 0, 4, 3, 2, 1, 0}, shadowMap);
+            assertArrayEquals(    new int[]{3, 4, 5, 6, 0, 1, 4, 0, 0, 0}, shadowMap);
         }
     }
 
