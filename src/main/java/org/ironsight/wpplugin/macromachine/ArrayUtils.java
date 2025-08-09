900d3115e1fccc34cdb375edfabeb0ab71f15aae
IR0NSIGHT <irnsght@googlemail.com>
2025-08-09 14:28:05 +0200

    more robust loggin

--- src/main/java/org/ironsight/wpplugin/macromachine/ArrayUtils.java
+++ src/main/java/org/ironsight/wpplugin/macromachine/ArrayUtils.java
@@ -2,6 +2,8 @@ package org.ironsight.wpplugin.macromachine;
 
 public class ArrayUtils {
     public static int findMin(int[] array) {
+        if (array.length == 0)
+            return Integer.MAX_VALUE;
         int min = array[0];
         for (int i = 1; i < array.length; i++) {
             if (array[i] < min) {
@@ -12,6 +14,8 @@ public class ArrayUtils {
     }
 
     public static int findMax(int[] array) {
+        if (array.length == 0)
+            return Integer.MIN_VALUE;
         int max = array[0];
         for (int i = 1; i < array.length; i++) {
             if (array[i] > max) {
