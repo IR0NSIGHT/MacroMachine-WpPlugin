1f48067c34a5dbdc93ff9c1fdc990ab7d7e2100c
IR0NSIGHT <irnsght@googlemail.com>
2026-05-08 19:47:43 +0200

    update hooks to skip if no relevant files are staged

--- precommit.sh
+++ precommit.sh
@@ -1,2 +1,8 @@
 #!/bin/sh
 echo "precommit hook - backend"
+if ! git diff --cached --name-only | grep -q '^src/main/java/'; then
+    echo "No Java files changed, skipping"
+    exit 0
+fi
+
+mvn clean test
