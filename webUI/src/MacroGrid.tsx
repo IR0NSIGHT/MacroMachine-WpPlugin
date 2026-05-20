1bb9f427733d9482fb72eadcbc7dc74e4c0ec5e0
Ir0nsight <irnsght@googlemail.com>
2026-05-20 15:21:45 +0200

    extract MacroGrid component

---
+++ webUI/src/MacroGrid.tsx
@@ -0,0 +1,177 @@
+import { Box, Tooltip, FormControlLabel, Switch, Grid } from "@mui/material";
+import { useState } from "react";
+import { postQueueMacros } from "./API/fetch";
+import { ActionDetailsDialog } from "./components/MacroList/ActionDetailsDialog";
+import MacroCard from "./components/MacroList/MacroCard";
+import { MacroDetailsDialog } from "./components/MacroList/MacroDetailsDialog";
+import { MacroDTO, ActionDTO, ExecutionStateDTO, isMacroDTO } from "./types/DTO";
+import Item from "@mui/material/Grid";
+
+const imageURLs = [
+  "https://images.unsplash.com/photo-1506744038136-46273834b3fb?q=80&w=1600&auto=format&fit=crop",
+  "https://images.unsplash.com/photo-1500530855697-b586d89ba3ee?q=80&w=1600&auto=format&fit=crop",
+  "https://images.unsplash.com/photo-1470770841072-f978cf4d019e?q=80&w=1600&auto=format&fit=crop",
+  "https://images.unsplash.com/photo-1441974231531-c6227db76b6e?q=80&w=1600&auto=format&fit=crop",
+  "https://images.unsplash.com/photo-1501785888041-af3ef285b470?q=80&w=1600&auto=format&fit=crop",
+  "https://images.unsplash.com/photo-1439066615861-d1af74d74000?q=80&w=1600&auto=format&fit=crop",
+  "https://images.unsplash.com/photo-1464822759023-fed622ff2c3b?q=80&w=1600&auto=format&fit=crop",
+  "https://images.unsplash.com/photo-1500375592092-40eb2168fd21?q=80&w=1600&auto=format&fit=crop",
+  "https://images.unsplash.com/photo-1433086966358-54859d0ed716?q=80&w=1600&auto=format&fit=crop",
+  "https://images.unsplash.com/photo-1426604966848-d7adac402bff?q=80&w=1600&auto=format&fit=crop",
+  "https://images.unsplash.com/photo-1447752875215-b2761acb3c5d?q=80&w=1600&auto=format&fit=crop",
+  "https://images.unsplash.com/photo-1443890923422-7819ed4101c0?q=80&w=1600&auto=format&fit=crop",
+  "https://images.unsplash.com/photo-1507525428034-b723cf961d3e?q=80&w=1600&auto=format&fit=crop",
+  "https://images.unsplash.com/photo-1493246507139-91e8fad9978e?q=80&w=1600&auto=format&fit=crop",
+  "https://images.unsplash.com/photo-1469474968028-56623f02e42e?q=80&w=1600&auto=format&fit=crop",
+  "https://images.unsplash.com/photo-1451187580459-43490279c0fa?q=80&w=1600&auto=format&fit=crop",
+  "https://images.unsplash.com/photo-1418065460487-3e41a6c84dc5?q=80&w=1600&auto=format&fit=crop",
+  "https://images.unsplash.com/photo-1502082553048-f009c37129b9?q=80&w=1600&auto=format&fit=crop",
+  "https://images.unsplash.com/photo-1448375240586-882707db888b?q=80&w=1600&auto=format&fit=crop",
+  "https://images.unsplash.com/photo-1472396961693-142e6e269027?q=80&w=1600&auto=format&fit=crop",
+  "https://images.unsplash.com/photo-1465146344425-f00d5f5c8f07?q=80&w=1600&auto=format&fit=crop",
+  "https://images.unsplash.com/photo-1431794062232-2a99a5431c6c?q=80&w=1600&auto=format&fit=crop",
+  "https://images.unsplash.com/photo-1500534314209-a25ddb2bd429?q=80&w=1600&auto=format&fit=crop",
+  "https://images.unsplash.com/photo-1458668383970-8ddd3927deed?q=80&w=1600&auto=format&fit=crop",
+  "https://images.unsplash.com/photo-1472214103451-9374bd1c798e?q=80&w=1600&auto=format&fit=crop",
+  "https://images.unsplash.com/photo-1501594907352-04cda38ebc29?q=80&w=1600&auto=format&fit=crop",
+  "https://images.unsplash.com/photo-1449034446853-66c86144b0ad?q=80&w=1600&auto=format&fit=crop",
+  "https://images.unsplash.com/photo-1465189684280-6a8fa9b19a7a?q=80&w=1600&auto=format&fit=crop",
+  "https://images.unsplash.com/photo-1504208434309-cb69f4fe52b0?q=80&w=1600&auto=format&fit=crop",
+  "https://images.unsplash.com/photo-1505761671935-60b3a7427bad?q=80&w=1600&auto=format&fit=crop",
+];
+
+export function MacroGrid({
+  macros,
+  actions,
+  executionState,
+}: {
+  macros: MacroDTO[];
+  actions: ActionDTO[];
+  executionState: ExecutionStateDTO;
+}) {
+  const [hideNested, setHideNested] = useState(false);
+  const [viewedMacro, setViewedMacro] = useState<
+    (MacroDTO & { steps: (ActionDTO | MacroDTO)[] }) | null
+  >(null);
+  const [viewAction, setViewAction] = useState<ActionDTO | null>(null);
+
+  const macroSet = new Set<string>();
+  macros.forEach((m) => macroSet.add(m.uid));
+
+  const uuidToMacroOrAction = new Map<string, MacroDTO | ActionDTO>();
+  macros.forEach((macro) => uuidToMacroOrAction.set(macro.uid, macro)); //FIXME useMemo ? or sth?
+  actions.forEach((action) => uuidToMacroOrAction.set(action.uid, action));
+
+  const nestedMacroUIDs = new Set<string>();
+  macros.forEach((macro) =>
+    macro.executionUUIDs
+      .filter((uid) => macroSet.has(uid))
+      .forEach((uid) => nestedMacroUIDs.add(uid)),
+  ); //FIXME useMemo ? or sth?
+
+  const onShare = (macro: MacroDTO) => {
+    console.log("USER WANTS TO SHARE THE MARCO", macro.name);
+  };
+
+  const onEdit = (macro: MacroDTO) => {
+    console.log("USER WANTS TO EDIT THE MARCO", macro.name);
+  };
+
+  const onView = (macro: MacroDTO) => {
+    const steps: (MacroDTO | ActionDTO)[] = macro.executionUUIDs.map(
+      (uid) => uuidToMacroOrAction.get(uid)!,
+    );
+    const macroWithSteps = { ...macro, steps: steps };
+    console.log("fat mamcro:", macroWithSteps);
+    console.log("actions:", uuidToMacroOrAction);
+    setViewedMacro(macroWithSteps);
+  };
+
+  const filterHideNested = (macro: MacroDTO) => {
+    const isNested = nestedMacroUIDs.has(macro.uid);
+    return hideNested ? !isNested : true;
+  };
+  console.log(nestedMacroUIDs);
+
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        height: "95vh", // FIXME ugly hack to make the fucking flexbox work
+      }}
+      p={1}
+    >
+      <Tooltip title="Hide all macros, that are used by another macro.">
+        <FormControlLabel
+          control={
+            <Switch
+              checked={hideNested}
+              onChange={(event) => {
+                setHideNested(event.target.checked);
+              }}
+            />
+          }
+          label={"Hide " + nestedMacroUIDs.size + " nested macro(s)"}
+        />
+      </Tooltip>
+      <Box
+        sx={{
+          flex: 1,
+          minHeight: 0,
+          overflowY: "auto",
+          p: 2,
+        }}
+      >
+        <Grid container spacing={{ xs: 1, md: 4 }}>
+          {macros.filter(filterHideNested).map((macro, idx) => (
+            <Grid
+              key={macro.uid}
+              size={{
+                xs: 12,
+                sm: 6,
+                md: 4,
+                lg: 3,
+              }}
+            >
+              <Item>
+                <MacroCard
+                  macro={macro}
+                  execution={executionState}
+                  imageURL={imageURLs[idx % imageURLs.length]}
+                  onRequestExecution={() =>
+                    postQueueMacros([macro.uid])
+                      .then(console.log)
+                      .catch(console.error)
+                  }
+                  onView={() => onView(macro)}
+                  onShare={() => onShare(macro)}
+                  onEdit={() => onEdit(macro)}
+                />
+              </Item>
+            </Grid>
+          ))}
+        </Grid>
+
+        <MacroDetailsDialog
+          open={!!viewedMacro}
+          macro={viewedMacro}
+          onClose={() => setViewedMacro(null)}
+          onViewItem={(item: MacroDTO | ActionDTO) => {
+            if (isMacroDTO(item)) {
+              onView(item);
+            } else {
+              setViewAction(item)
+            }
+          }}
+        />
+        <ActionDetailsDialog
+          open={!!viewAction}
+          action={viewAction}
+          onClose={() => setViewAction(null)}
+          onViewItem={() => {}}
+        />
+      </Box>
+    </Box>
+  );
+}
