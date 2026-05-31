f7978899f77e7d1207cbbb20bd81e5a8c77e818e
IR0NSIGHT <irnsght@googlemail.com>
2026-05-31 12:18:48 +0200

    fix: run_backend.sh for windows

--- run_backend.sh
+++ run_backend.sh
@@ -1,2 +1,7 @@
-mvn dependency:copy-dependencies
-java -cp "target/classes:target/dependency/*" org.ironsight.wpplugin.macromachine.WebUIServer
+#!/usr/bin/env bash
+set -e
+
+echo "Build and Start MacroMachine Backend Server"
+
+mvn compile dependency:copy-dependencies
+java -cp "target/classes;target/dependency/*" org.ironsight.wpplugin.macromachine.WebUIServer
