9cc100a1cdd482c7358a2687287f49a8e71b7d95
Ir0nsight <irnsght@googlemail.com>
2026-06-28 15:14:52 +0200

    dont wait for debug attach on running in test mode

--- run_wp.sh
+++ run_wp.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-mvn test -PtestWithWorldpainter -Dmaven.surefire.debug="-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005"
+mvn test -PtestWithWorldPainter -Dmaven.surefire.debug="-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005"
